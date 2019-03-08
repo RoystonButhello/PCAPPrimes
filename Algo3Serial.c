80cdf099a2104905034b06ec889934e6883ee0f9
Royston Buthello <43342513+RoystonButhello@users.noreply.github.com>
2019-03-08 15:58:14 +0530

    Update Algo3Serial.c

--- Algo3Serial.c
+++ Algo3Serial.c
@@ -14,7 +14,7 @@ integer power(integer val, integer exp)
 
 integer bound(integer n)
 {
-	short size = sizeof(integer) * 8 - 1;
+	short size = sizeof(integer) * 8;
 	integer limit = power(2, size) - power(2, (size / 2) + 1) - 1;
 	if (n > limit)
 	{
@@ -26,14 +26,13 @@ integer bound(integer n)
 
 int main()
 {
-	//Datatype Range:		[0,4294967296]
-	//Acceptable i/p Range: [1,2147483648]	
+	//Range:		[0,4294836223]
 	integer n; printf("Enter limit: "); scanf("%u", &n);
 	n = bound(n);
-
+	int len = n/2;
 
 	//Boolean array initialized to false
-	_Bool *C = (_Bool *)calloc(n, sizeof(_Bool));
+	_Bool *C = (_Bool *)calloc(len, sizeof(_Bool));
 	if (C == NULL) { printf("Memory Allocation Failed!\n"); exit(1); }
 	integer i, j;
 
@@ -42,18 +41,18 @@ int main()
 	double  CPU_TIME;
 
 	START_TIME = clock();
-	for (i = 3; i <= (integer)pow(2 * n, 0.5); i += 2)					//i->[3,5,7,9...,sqrt(2*n)] | i corresponds to C[(i-3)/2]
+	for (i = 3; i <= (integer)pow(n, 0.5); i += 2)					//i->[3,5,7,9...,sqrt(n)] | i corresponds to C[(i-3)/2]
 	{
 		if (C[(i - 3) / 2] == 0)		//false implies <i> is prime
 		{
-			for (j = (integer)pow(i, 2); j <= 2 * n; j += (2 * i))	//j->[i^2,2*n] | increments by 2*i
+			for (j = (integer)pow(i, 2); j <= n; j += (2 * i))	//j->[i^2,n] | increments by 2*i
 				C[(j - 3) / 2] = 1;
 			/*LOGIC
 			Instead of using modulus operation to check if it is prime within the j-loop,
 			change increment from "2" to "2i" and directly mark them as non-prime
 			"i^2 + 2k*i" is essentially "m*i", i.e., it is a multiple of i for sure
 			*/
-			//printf("%u stopped at %u\n", i, j);
+			printf("%u stopped at %u\n", i, j - 2 * i);
 		}
 	}
 	END_TIME = clock();
@@ -62,7 +61,7 @@ int main()
 	//Print all primes found
 	short count = 1;
 	printf("\nPrimes in range:\n");
-	for(i=0; i<n; i++)
+	for(i=0; i<len; i++)
 	{
 		if (C[i] == 0)	  {printf("%u\t",2*i+3); count++;}
 		if (count%10 == 0) { printf("\n"); count /= 10;}
@@ -71,7 +70,7 @@ int main()
 
 	/*
 	//Print last prime found
-	for(i=n-1; i>=0; i--)
+	for(i=len-1; i>=0; i--)
 		if (C[i] == 0) break;
 	printf("Largest prime in range: %u\n",2*i+3);
 	*/
@@ -79,7 +78,7 @@ int main()
 
 	printf("\nLast 10 primes:\n");
 	short count = 0;
-	for (i = n - 1; count < 10; i--)
+	for (i = len - 1; count < 10; i--)
 	{
 		if (C[i] == 0)
 		{
